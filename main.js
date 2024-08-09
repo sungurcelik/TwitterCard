362ea5c976d12f801decd2bc2e60040a054711b8
salihsungurcelik <salihcelik099@gmail.com>
2024-08-09 21:08:34 +0300

    finis TwitterCard

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const placeholder = document.querySelector('.placeholder')
 const editableInput = document.querySelector('.editable')
+const tweetButton = document.querySelector('.button')
+const counter = document.getElementById('counter')
+const readOnly = document.querySelector('.readonly')
 
 editableInput.addEventListener('click', () => {
     placeholder.style.color = "#98a5b1"
@@ -15,13 +18,34 @@ editableInput.onkeypress = (e) => {
 }
 
 editableInput.onkeyup = (e) => {
+    placeholder.style.display = 'none'
     inputValidate(e.target.innerText)
 }
 
 const inputValidate = (tweet) => {
-    console.log(tweet.length)
     const tweetLength = tweet.length
+    const tweetLimit = 5
+    const currentLimit = tweetLimit - tweetLength
     if (tweetLength <= 0) {
         placeholder.style.display = 'block'
+        tweetButton.classList.remove('active')
+        counter.style.display = 'none'
+    } else {
+        tweetButton.classList.add('active')
+        counter.style.display = 'block'
+        counter.innerText = currentLimit
     }
+    let newTweet
+    if (tweetLength > tweetLimit) {
+        let overTweet = tweet.substr(tweetLimit, tweetLength)
+        let overTweetElement = `<span class='overTweet'>${overTweet}</span>`
+        newTweet = tweet.substr(0, tweetLimit) + overTweetElement
+        readOnly.style.zIndex = '1'
+        counter.style.color = 'red'
+        tweetButton.classList.remove('active')
+    } else {
+        counter.style.color = '#333'
+        readOnly.style.zIndex = '-5'
+    }
+    readOnly.innerHTML = newTweet
 }
